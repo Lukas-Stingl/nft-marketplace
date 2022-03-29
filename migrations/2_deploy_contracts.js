d526738b796086af5d2c95910bc9326ca4b0d217
AlwinFassbender <alwin.fassbender@gmail.com>
2022-03-29 14:44:13 +0200

    update contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 const NFTCollection = artifacts.require("NFTCollection");
 const NFTMarketplace = artifacts.require("NFTMarketplace");
-const HelloWorld = artifacts.require("HelloWorld");
+
 
 module.exports = async function (deployer) {
   await deployer.deploy(NFTCollection);
 
   const deployedNFT = await NFTCollection.deployed();
   const NFTAddress = deployedNFT.address;
-  await deployer.deploy(NFTMarketplace, NFTAddress, HelloWorld);
+  await deployer.deploy(NFTMarketplace, NFTAddress);
 };
--- src/contracts/HelloWorld.sol
+++ src/contracts/HelloWorld.sol
@@ -3,6 +3,6 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
     function renderHelloWorld() public pure returns (string memory) {
-        return "helloWorld";
+        return "Hello World!";
     }
 }
