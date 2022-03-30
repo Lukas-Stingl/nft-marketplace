51def3d0542603fc5bf872d72bc7121b2c816d31
paablos <uuqdr@student.kit.edu>
2022-03-30 12:25:05 +0200

    Collection Test Layout

--- src/components/Content/NFTCollection/Collection.js
+++ src/components/Content/NFTCollection/Collection.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import './Collection_styles.css'; //tell this .js to use the Stylessheet
 
 function Collection() {
-    return <h2>About</h2>;
+    return (
+    <html>
+    <head>
+    </head>
+    <style>
+    </style>
+        <body>
+    <section class="heading">
+            <h2 align="center">
+                NFT Collection
+            </h2>
+            <p align="center">
+                Explore the World of NFTs
+            </p>
+    </section>
+  <section class="cms-boxes">
+          <div class="cms-boxes-items one">
+                <i class="fa fa-4x fa-laptop">&nbsp;</i>
+                <h2>NFT #1</h2>
+                <p>Cat</p>
+          </div>
+          <div class="cms-boxes-items two">
+                <i class="fa fa-4x fa-cog">&nbsp;</i>
+                <h2> NFT #2</h2>
+                <p>Human</p>
+        </div>
+          <div class="cms-boxes-items three">
+                <i class="fa fa-4x fa-cog">&nbsp;</i>
+                <h2>NFT #3</h2>
+                <p>Dog</p>
+         </div>
+          <div class="cms-boxes-items four">
+                <i class="fa fa-4x fa-cog">&nbsp;</i>
+                <h2>NFT #4</h2>
+                <p>Rabbit</p>
+          </div>
+          <div class="cms-boxes-items five">
+                <i class="fa fa-4x fa-cog">&nbsp;</i>
+                <h2>NFT #5</h2>
+                <p>Horse</p>
+          </div>
+          <div class="cms-boxes-items six">
+                <i class="fa fa-4x fa-cog">&nbsp;</i>
+                <h2>NFT #6</h2>
+                <p>Horse</p>
+          </div>
+          <div class="cms-boxes-items seven">
+                <i class="fa fa-4x fa-cog">&nbsp;</i>
+                <h2>NFT #7</h2>
+                <p>Horse</p>
+          </div>
+          <div class="cms-boxes-items eight">
+                <i class="fa fa-4x fa-cog">&nbsp;</i>
+                <h2>NFT #8</h2>
+                <p>Horse</p>
+          </div>
+  </section>
+        </body>
+    </html>   
+    );
 }
 
 export default Collection;
