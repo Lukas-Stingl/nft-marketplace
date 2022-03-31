0271eab5f541de167572ca90837dbf75451084fa
AlwinFassbender <alwin.fassbender@gmail.com>
2022-03-31 13:07:50 +0200

    mint nft layout

--- src/components/Content/MintNFT/CreateNFT.js
+++ src/components/Content/MintNFT/CreateNFT.js
@@ -3,20 +3,30 @@ import "./mint.css"
 
 function Create() {
     return (
-        <div class="createform">
-            <div className="w-1/2 flex flex-col pb-12">
+        <section class="createform">
+            <h1>FIOEZGBCOEIUZGBFOUI</h1>
+            <h3>Title</h3>
+            <h4>Subtitle</h4>
+            <div>
                 <input
                     placeholder="Asset Name"
                     className="mt-8 border rounded p-4"
                     onChange={e => {
-                    }}
-                />
+                    }} />
+            </div>
+            <h3>Title</h3>
+            <h4>Subtitle</h4>
+            <div>
                 <textarea
                     placeholder="Asset description"
                     className="mt-2 border rounded p-4"
                     onChange={e => {
                     }}
                 />
+            </div>
+            <h3>Title</h3>
+            <h4>Subtitle</h4>
+            <div>
                 <input
                     placeholder="Asset Price in Eth"
                     className="mt-8 border rounded p-4"
@@ -24,6 +34,10 @@ function Create() {
                     onChange={e => {
                     }}
                 />
+            </div>
+            <h3>Title</h3>
+            <h4>Subtitle</h4>
+            <div>
                 <input
                     type="file"
                     name="Asset"
@@ -31,26 +45,31 @@ function Create() {
                     onChange={e => {
                     }}
                 />
-                {/* {
-              fileUrl && (
-    
-                <Image
-                  src={fileUrl}
-                  alt="Picture of the author"
-                  className="rounded mt-4"
-                  width={350}
-                  height={500}
-                // blurDataURL="data:..." automatically provided
-                // placeholder="blur" // Optional blur-up while loading
-                />
-              )
-            } */}
+            </div>
+            {/* {
+                    fileUrl && (
+
+                    <Image
+                        src={fileUrl}
+                        alt="Picture of the author"
+                        className="rounded mt-4"
+                        width={350}
+                        height={500}
+                    // blurDataURL="data:..." automatically provided
+                    // placeholder="blur" // Optional blur-up while loading
+                    />
+                    )
+                } */}
+
+            <div>
                 <button onClick={e => {
                 }}
-                    className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
-                >Create NFT</button>
+
+                >Create NFT
+                </button>
             </div>
-        </div>);
+        </section>
+    );
 }
 
 export default Create;
