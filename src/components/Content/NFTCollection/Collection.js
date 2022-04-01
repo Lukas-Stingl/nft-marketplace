1ba60de7058f164d462a31aaf8e3fc3446b5e192
paablos <uuqdr@student.kit.edu>
2022-04-01 12:23:23 +0200

    work Collection Layout

--- src/components/Content/NFTCollection/Collection.js
+++ src/components/Content/NFTCollection/Collection.js
@@ -18,50 +18,51 @@ function Collection() {
                 Explore the World of NFTs
             </p>
     </section>
-  <section class="cms-boxes">
-          <div class="cms-boxes-items one">
+  <section class="boxes">
+          <div class="boxes-items one">
                 <div class="nft">
                 <i class="fa fa-4x fa-laptop">&nbsp;</i>
                 <img src={eth} width="100" height="100" class="center"></img>
                 </div>
                   <p class="name">Cat</p>
+                  <p class="price"> 1 ETH </p>
           </div>
-          <div class="cms-boxes-items two">
+          <div class="boxes-items two">
                 <i class="fa fa-4x fa-cog">&nbsp;</i>
                 <h2> NFT #2</h2>
                 <p class="name">Human</p>
         </div>
-          <div class="cms-boxes-items three">
+          <div class="boxes-items three">
                 <i class="fa fa-4x fa-cog">&nbsp;</i>
                 <h2>NFT #3</h2>
                 <p class="name">Dog</p>
          </div>
-          <div class="cms-boxes-items four">
+          <div class="boxes-items four">
                 <i class="fa fa-4x fa-cog">&nbsp;</i>
                 <h2>NFT #4</h2>
                 <p class="name">Rabbit</p>
           </div>
-          <div class="cms-boxes-items five">
+          <div class="boxes-items five">
                 <i class="fa fa-4x fa-cog">&nbsp;</i>
                 <h2>NFT #5</h2>
                 <p class="name">Horse</p>
           </div>
-          <div class="cms-boxes-items six">
+          <div class="boxes-items six">
                 <i class="fa fa-4x fa-cog">&nbsp;</i>
                 <h2>NFT #6</h2>
                 <p class="name">Horse</p>
           </div>
-          <div class="cms-boxes-items seven">
+          <div class="boxes-items seven">
                 <i class="fa fa-4x fa-cog">&nbsp;</i>
                 <h2>NFT #7</h2>
                 <p class="name">Horse</p>
           </div>
-          <div class="cms-boxes-items eight">
+          <div class="boxes-items eight">
                 <i class="fa fa-4x fa-cog">&nbsp;</i>
                 <h2>NFT #8</h2>
                 <p class="name">Horse</p>
           </div>
-          <div class="cms-boxes-items nine">
+          <div class="boxes-items nine">
                 <i class="fa fa-4x fa-cog">&nbsp;</i>
                 <h2>NFT #9</h2>
                 <p class="name">Horse</p>
