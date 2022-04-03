d4a8b626f1663559cf22fd2a001cbc9b29a1345a
SchimSlady <you@example.comtschaible@web.de>
2022-04-03 20:21:43 +0200

    First Layout "Home"

--- src/components/Content/Home.js
+++ src/components/Content/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import "./Home.css"
 
 function Home() {
-    return (<h2>Home</h2>);
+
+    return (
+        <div>
+            <div class="Buttons">
+                <h1>Welcome to the world of NFTs! </h1>
+                <h2>Discover, collect, and sell extraordinary NFTs.</h2>
+                <a href="../collection" >
+                    <button class="Discover">Discover </button>
+                </a>
+                <div class="divider" />
+                <a href="../create">
+                    <button type="button" class="Create">Create NFTs</button>
+                </a>
+            </div>
+
+            <div class="NFT_Vorschau">
+            </div>
+        </div>
+    );
 }
 
 export default Home;
