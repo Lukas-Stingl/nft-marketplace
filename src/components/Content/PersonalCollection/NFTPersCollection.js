af960747d8fc05cf5f1917f30b7e65b98194a782
davidvblumenthal <davidvblumenthal@gmail.com>
2022-04-03 23:19:13 +0200

    add navbar to perscollection

--- src/components/Content/PersonalCollection/NFTPersCollection.js
+++ src/components/Content/PersonalCollection/NFTPersCollection.js
@@ -73,9 +73,16 @@ const NFTPersCollectionPage = () => {
   }
 
   return (
-   <html>
+   <>
+    
+    <section class="heading">
+
+    </section>
+
         <section class="profile">
-        <header class="header">
+ 
+
+        <section class="header">
             <div class="details">
             <img src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=b38c22a46932485790a3f52c61fcbe5a" alt="John Doe" class="profile-pic"/>
             <h1 class="heading">{web3Ctx.account}</h1>
@@ -95,8 +102,9 @@ const NFTPersCollectionPage = () => {
                 </div>
             </div>
             </div>
-        </header>
+            </section>
         </section>
+    
     <div className="row text-center">
        
       {collectionCtx.collection.map((NFT, key) => {
@@ -151,7 +159,7 @@ const NFTPersCollectionPage = () => {
         );
       })}
     </div>
-    </html>
+    </>
   );
 };
 
